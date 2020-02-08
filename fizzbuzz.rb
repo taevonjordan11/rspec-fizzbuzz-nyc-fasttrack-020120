e79f282c6e16703ce2f41c63b2abdad6d76a6a90
agile-proc-6339 <taevonjordan1@gmail.com>
2020-02-08 19:53:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -16,3 +16,15 @@ class Integer
     result == "" ? self : result
   end
 end
+  
+def fizzbuzz(num)
+  if (num % 5 == 0) && (num % 3 == 0)
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return
+  end
+end
